1e2b66cb4937a72ba7f5d18bfed2bb123f1fa8e6
nosh1114 <114987002+nosh1114@users.noreply.github.com>
2025-05-21 14:29:34 +0000

    rails generate controller StaticPages home help

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+class StaticPagesController < ApplicationController
+  def home
+    # =>app/views/static_pages/home.html.erbが標準で表示される
+    # render ...とすると、指定できる。
+  end
+
+  # app/views/コントローラー名/メソッド名.html.erb
+  def help
+    # roots / rootは、ルートパスを指します。
+    # routes / routeは、URLとコントローラーのアクションを結びつけるものです。
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # コントローラー名/メソッド
+  get 'static_pages/home'
+  get 'static_pages/help'
   root "hello#index"
 end

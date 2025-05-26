fba2d2d2712a3e4fb3e6bdd2e966edae45c68833
nosh1114 <114987002+nosh1114@users.noreply.github.com>
2025-05-26 10:23:09 +0000

    create user and password

---
+++ test/models/user_test.rb
@@ -0,0 +1,56 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: 'Example User', email: 'sample.@example.com', password: 'foobar123',
+                     password_confirmation: 'foobar123')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'name should be present' do
+    @user.name = '     '
+    assert_not @user.valid?
+  end
+
+  test ' name should not be too long' do
+    # 文字の掛け算
+    @user.name = 'a' * 51
+    # This line checks if the name exceeds the maximum length
+    assert_not @user.valid?
+  end
+
+  test 'email should not be too long' do
+    @user.email = 'a' * 244 + '@example.com'
+    # This line checks if the email address exceeds the maximum length
+    assert_not @user.valid?
+  end
+
+  test 'email validation should accept valid addresses' do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test 'email addresses should be unique' do
+    duplicate_user = @user.dup
+    # @userは一旦セーブする。
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test 'password should be present (nonblank)' do
+    @user.password = @user.password_confirmation = ' ' * 8
+    assert_not @user.valid?
+  end
+
+  test 'password should have a minimum length' do
+    @user.password = @user.password_confirmation = 'a' * 7
+    assert_not @user.valid?
+  end
+end

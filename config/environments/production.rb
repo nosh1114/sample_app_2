aa64da6baca2cabbcc002e054f9e4df5ea622581
nosh1114 <nao.shiba.public@gmail.com>
2025-06-05 18:48:13 +0900

    adsf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://sample-app-2.onrender.com'
+  # ここや！
+  host = 'https://sample_app_2-1.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
